d72d4c8f86c26942d3a72feb7085e7e8bff25358
Gibigabcsusz <gbr.szilagyi@gmail.com>
2022-04-30 20:24:45 +0200

    kezdetleges testbench-ek

--- HDMI_conv_filt/bram_delay_tb.v
+++ HDMI_conv_filt/bram_delay_tb.v
@@ -43,9 +43,53 @@ module bram_delay_tb ();
   end
 
   initial begin
-  // add stimulus here
+  // A pixelek késleltetését letesztelni elég egyszerű,
+  // egy számlálót kell beadni neki
+  // és összehasonlítani a kimenet eltolódását a bemenethez képest
+    din = 0;
+    #2
+    forever #2 din = din + 1;
   end
 
 
+  integer i;
+  initial begin
+  // A status jelek késleltetését jellegzetes impulzusszélességekkel
+  // lehet jól ellenőrizni
+    st_i[0]=0;
+    for (i=2; i<500; i=i+2) begin
+      #i
+      st_i[0]=1;
+      #i
+      st_i[0]=0;
+    end
+  end
+
+  integer j;
+  initial begin
+  // A status jelek késleltetését jellegzetes impulzusszélességekkel
+  // lehet jól ellenőrizni
+    st_i[1]=0;
+    for (j=2; j<500; j=j+2) begin
+      #j
+      st_i[1]=1;
+      #j
+      st_i[1]=0;
+    end
+  end
+
+  integer k;
+  initial begin
+  // A status jelek késleltetését jellegzetes impulzusszélességekkel
+  // lehet jól ellenőrizni
+    st_i[2]=0;
+    #2
+    for (k=2; k<500; k=k+2) begin
+      #k
+      st_i[2]=1;
+      #k
+      st_i[2]=0;
+    end
+  end
 
 endmodule
