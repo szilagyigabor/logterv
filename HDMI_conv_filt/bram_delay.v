5deb2f4cfeb4b25f708888b15ab55f2bf0e986db
Bálint Gergely <gergely@balintgergely.hu>
2022-05-03 23:46:44 +0200

    address calc

--- HDMI_conv_filt/bram_delay.v
+++ HDMI_conv_filt/bram_delay.v
@@ -15,6 +15,9 @@ module bram_delay (
 );
 wire hs = stat_in[1];
 
+// counter to set picture line length
+// current line is delayed according to
+// the length of the previous line
 reg [11:0] pic_width;
 reg [11:0] pic_w_cntr;
 reg [11:0] pic_w_cntr_prev;
@@ -29,13 +32,26 @@ always @(posedge clk) begin
   pic_w_cntr_prev <= pic_w_cntr;
 end
 
+// calculate address for single port blockrams
+// to work cyclically
+reg [10:0] addr_reg; //TODO: hossz = log2 sp_ram hossz
+always @(posedge clk) begin
+  if (rst) begin
+    addr_reg <= 0;
+  end else if ( addr_reg == pic_width ) begin
+    addr_reg <= 0;
+  end else begin
+    addr_reg <= addr_reg + 1;
+  end
+end
+
 
 wire [26:0] del0_out;
 sp_ram bram_delay0(
   .clk(clk),
   .en(1),
   .we(1),
-  .addr(),
+  .addr(addr_reg),
   .din({stat_in, data_in}),
   .dout(del0_out)
 );
@@ -45,7 +61,7 @@ sp_ram bram_delay1(
   .clk(clk),
   .en(1),
   .we(1),
-  .addr(),
+  .addr(addr_reg),
   .din(del0_out),
   .dout(del1_out)
 );
@@ -55,7 +71,7 @@ sp_ram bram_delay2(
   .clk(clk),
   .en(1),
   .we(1),
-  .addr(),
+  .addr(addr_reg),
   .din(del1_out),
   .dout(del2_out)
 );
@@ -64,7 +80,7 @@ sp_ram bram_delay3(
   .clk(clk),
   .en(1),
   .we(1),
-  .addr(),
+  .addr(addr_reg),
   .din(del2_out),
   .dout(del3_out)
 );
