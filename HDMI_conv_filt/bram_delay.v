ddb2a5d99cb1aebaba9e8e09135c16d6834c4566
Bálint Gergely <gergely@balintgergely.hu>
2022-05-04 14:10:12 +0200

    rework bram delay by separating colors, set correct sp_ram sizes

--- HDMI_conv_filt/bram_delay.v
+++ HDMI_conv_filt/bram_delay.v
@@ -3,93 +3,62 @@
 module bram_delay (
   input         clk,
   input         rst,
-  input  [23:0] data_in,
-  input  [2:0]  stat_in,
+  input  [7:0]  data_in,
+  input         stat_in,
+  input  [11:0] addr,
 
-  output [23:0] pa,
-  output [23:0] pb,
-  output [23:0] pc,
-  output [23:0] pd,
-  output [23:0] pe,
-  output [2:0]  stat_o
+  output [7:0]  pa,
+  output [7:0]  pb,
+  output [7:0]  pc,
+  output [7:0]  pd,
+  output [7:0]  pe,
+  output        stat_o
 );
-wire hs = stat_in[1];
 
-// counter to set picture line length
-// current line is delayed according to
-// the length of the previous line
-reg [11:0] pic_width;
-reg [11:0] pic_w_cntr;
-reg [11:0] pic_w_cntr_prev;
-always @(posedge clk) begin
-  if (rst) begin
-    pic_w_cntr <= 0;
-    pic_w_cntr_prev <= 0;
-  end else if (hs) begin
-    pic_width <= pic_w_cntr_prev;
-    pic_w_cntr <= 0;
-  end
-  pic_w_cntr_prev <= pic_w_cntr;
-end
-
-// calculate address for single port blockrams
-// to work cyclically
-reg [10:0] addr_reg; //TODO: hossz = log2 sp_ram hossz
-always @(posedge clk) begin
-  if (rst) begin
-    addr_reg <= 0;
-  end else if ( addr_reg == pic_width ) begin
-    addr_reg <= 0;
-  end else begin
-    addr_reg <= addr_reg + 1;
-  end
-end
-
-
-wire [26:0] del0_out;
+wire [8:0] del0_out;
 sp_ram bram_delay0(
   .clk(clk),
   .en(1),
   .we(1),
-  .addr(addr_reg),
+  .addr(addr),
   .din({stat_in, data_in}),
   .dout(del0_out)
 );
 
-wire [26:0] del1_out;
+wire [8:0] del1_out;
 sp_ram bram_delay1(
   .clk(clk),
   .en(1),
   .we(1),
-  .addr(addr_reg),
+  .addr(addr),
   .din(del0_out),
   .dout(del1_out)
 );
 
-wire [26:0] del2_out;
+wire [8:0] del2_out;
 sp_ram bram_delay2(
   .clk(clk),
   .en(1),
   .we(1),
-  .addr(addr_reg),
+  .addr(addr),
   .din(del1_out),
   .dout(del2_out)
 );
-wire [26:0] del3_out;
+wire [8:0] del3_out;
 sp_ram bram_delay3(
   .clk(clk),
   .en(1),
   .we(1),
-  .addr(addr_reg),
+  .addr(addr),
   .din(del2_out),
   .dout(del3_out)
 );
 
 assign pa = data_in;
-assign pb = del0_out[23:0];
-assign pc = del1_out[23:0];
-assign pd = del2_out[23:0];
-assign pe = del3_out[23:0];
-assign stat_o = del3_out[26:24];
+assign pb = del0_out[7:0];
+assign pc = del1_out[7:0];
+assign pd = del2_out[7:0];
+assign pe = del3_out[7:0];
+assign stat_o = del3_out[8];
 
 endmodule
--- HDMI_conv_filt/sp_ram.v
+++ HDMI_conv_filt/sp_ram.v
@@ -1,16 +1,16 @@
 `timescale 1ns / 1ps
-//TODO: parameters for depth, and width
 module sp_ram (
   input         clk,
   input         we,
   input         en,
-  input  [10:0] addr,
-  input  [7:0]  din,
-  output [7:0]  dout
+  input  [11:0] addr, // log2 4096
+  input  [8:0]  din,
+  output [8:0]  dout
 );
 
-reg [7:0] memory[2047:0];
-reg [7:0] dout_reg;
+// 8 bit pixel value + 1 status bit
+reg [8:0] memory[4095:0];
+reg [8:0] dout_reg;
 
 always @(posedge clk) begin
   if (en) begin
